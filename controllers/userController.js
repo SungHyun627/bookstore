2984a31cc822a5ca8fcf7673bedfe210d9e2122e
Sunghyun627 <kksscc4@gmail.com>
2024-01-12 08:53:57 +0900

    feat : 회원가입 시 이메일, 비밀번호 유효성 검사 기능 구현

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,6 @@ const signup = (req, res) => {
   const { email, password } = req.body;
   const sql = `INSERT INTO users (email, password) VALUES (?, ?)`;
   const values = [email, password];
-  console.log(email, password);
 
   connection.query(sql, values, (err, results) => {
     if (err) {
