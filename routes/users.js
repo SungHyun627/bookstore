09cac619c1c56af7128f788c04c20688c8cc00eb
Sunghyun627 <kksscc4@gmail.com>
2024-01-11 17:41:14 +0900

    feat : email, password 정보를 받아서 db에 저장하는 기능 구현

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../config/mysqlConfig');
+const { StatusCodes } = require('http-status-codes');
 
 router.use(express.json());
 
 // 회원가입
 router.post('/signup', (req, res) => {
-  res.json({
-    message: '회원가입',
+  const { email, password } = req.body;
+  const sql = `INSERT INTO users (email, password) VALUES (?, ?)`;
+  const values = [email, password];
+  console.log(email, password);
+
+  connection.query(sql, values, (err, results) => {
+    if (err) {
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    return res.status(StatusCodes.CREATED).json(results);
   });
 });
 
