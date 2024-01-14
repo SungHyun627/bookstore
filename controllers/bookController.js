c69e3a3676a5af1d14cb9b30ce8ee40078f6006d
Sunghyun627 <kksscc4@gmail.com>
2024-01-14 16:30:44 +0900

    feat: 카테고리 별 도서 목록 조회 기능 구현

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,13 +2,31 @@ const connection = require('../config/mysqlConfig');
 const { StatusCodes } = require('http-status-codes');
 
 const getAllBooksInfo = (req, res) => {
-  let sql = 'SELECT * FROM books';
-  connection.query(sql, (err, results) => {
-    if (err) {
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
-    return res.status(StatusCodes.CREATED).json(results);
-  });
+  let { category_id } = req.query;
+
+  if (category_id) {
+    let sql = 'SELECT * FROM books WHERE category_id=?';
+
+    connection.query(sql, category_id, (err, results) => {
+      if (err) {
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+
+      if (results.length) {
+        return res.status(StatusCodes.OK).json(results);
+      } else {
+        return res.status(StatusCodes.NOT_FOUND).end();
+      }
+    });
+  } else {
+    let sql = 'SELECT * FROM books';
+    connection.query(sql, (err, results) => {
+      if (err) {
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+      return res.status(StatusCodes.CREATED).json(results);
+    });
+  }
 };
 
 const getBookInfo = (req, res) => {
--- routes/books.js
+++ routes/books.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { body, param, validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 const { getAllBooksInfo, getBookInfo } = require('../controllers/bookController');
 
 router.use(express.json());
 
-// 전체 도서 조회
 router.get('/', getAllBooksInfo);
-
-// 개별 도서 조회
 router.get('/:id', getBookInfo);
-
-// 카테고리별 도서 목록 조회
-router.get('/', (req, res) => {
-  res.json({
-    message: '카테고리별 도서 목록 조회',
-  });
-});
 
 module.exports = router;
