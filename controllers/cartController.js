e332fa6ed134284971d298c275a70831ac5bc2e0
Sunghyun627 <kksscc4@gmail.com>
2024-01-21 12:29:01 +0900

    feat: TokenExipredError 처리 구현

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -6,7 +6,7 @@ const connection = require('../config/mysqlConfig');
 const addTocart = (req, res) => {
   const { book_id, quantity } = req.body;
 
-  const authorization = ensureAuthorization(req);
+  const authorization = ensureAuthorization(req, res);
 
   const sql = 'INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?);';
   const values = [book_id, quantity, authorization.id];
@@ -21,7 +21,7 @@ const addTocart = (req, res) => {
 
 const getCartItems = (req, res) => {
   const { selected } = req.body;
-  const authorization = ensureAuthorization(req);
+  const authorization = ensureAuthorization(req, res);
 
   const sql = `SELECT cartItems.id, book_id, title, summary, quantity, price 
                FROM cartItems LEFT JOIN books 
@@ -50,10 +50,16 @@ const removeCartItem = (req, res) => {
   });
 };
 
-const ensureAuthorization = (req) => {
-  const receivedJwt = req.headers.authorization;
-  const decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
-  return decodedJwt;
+const ensureAuthorization = (req, res) => {
+  try {
+    const receivedJwt = req.headers.authorization;
+    const decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+    return decodedJwt;
+  } catch (err) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: '로그인 세션이 만료되었습니다. 다시 로그인 하세요.',
+    });
+  }
 };
 
 module.exports = {
