123ab9e6891ca1910f83e18d47f46d6126beff45
Sunghyun627 <kksscc4@gmail.com>
2024-01-21 12:00:10 +0900

    feat: 좋아요 API Jwt 구현 완료

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,12 +1,16 @@
+const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes');
+const dotenv = require('dotenv');
 const connection = require('../config/mysqlConfig');
+
+dotenv.config();
 
 const addLike = (req, res) => {
   const { id } = req.params;
-  const { user_id } = req.body;
+  const authorization = ensureAuthorization(req);
 
   const sql = 'INSERT INTO likes (user_id, liked_book_id) VALUES (?, ?);';
-  const values = [user_id, id];
+  const values = [authorization.id, id];
 
   connection.query(sql, values, (err, results) => {
     if (err) {
@@ -17,11 +21,11 @@ const addLike = (req, res) => {
 };
 
 const removeLike = (req, res) => {
-  const { id } = req.params;
-  const { user_id } = req.body;
+  const book_id = req.params.id;
+  const authorization = ensureAuthorization(req);
 
   const sql = 'DELETE FROM likes WHERE user_id = ? AND liked_book_id = ?;';
-  const values = [user_id, id];
+  const values = [authorization.id, book_id];
 
   connection.query(sql, values, (err, results) => {
     if (err) {
@@ -29,6 +33,12 @@ const removeLike = (req, res) => {
     }
     return res.status(StatusCodes.OK).json(results);
   });
+};
+
+const ensureAuthorization = (req) => {
+  const receivedJwt = req.headers.authorization;
+  const decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+  return decodedJwt;
 };
 
 module.exports = { addLike, removeLike };
