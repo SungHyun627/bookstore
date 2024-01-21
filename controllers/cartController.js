6bdd7ec9aeec844504676a162fd5f7d439a73efa
Sunghyun627 <kksscc4@gmail.com>
2024-01-21 12:17:12 +0900

    feat: 장바구니 API Jwt 구현 완료

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,11 +1,15 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 const { StatusCodes } = require('http-status-codes');
 const connection = require('../config/mysqlConfig');
 
 const addTocart = (req, res) => {
-  const { book_id, quantity, user_id } = req.body;
+  const { book_id, quantity } = req.body;
+
+  const authorization = ensureAuthorization(req);
 
   const sql = 'INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?);';
-  const values = [book_id, quantity, user_id];
+  const values = [book_id, quantity, authorization.id];
 
   connection.query(sql, values, (err, results) => {
     if (err) {
@@ -16,13 +20,15 @@ const addTocart = (req, res) => {
 };
 
 const getCartItems = (req, res) => {
-  const { user_id, selected } = req.body;
+  const { selected } = req.body;
+  const authorization = ensureAuthorization(req);
+
   const sql = `SELECT cartItems.id, book_id, title, summary, quantity, price 
                FROM cartItems LEFT JOIN books 
                ON cartItems.book_id = books.id
                WHERE user_id=? AND cartItems.id IN (?)`;
 
-  const values = [user_id, selected];
+  const values = [authorization.id, selected];
   connection.query(sql, values, (err, results) => {
     if (err) {
       return res.status(StatusCodes.BAD_REQUEST).end();
@@ -32,16 +38,22 @@ const getCartItems = (req, res) => {
 };
 
 const removeCartItem = (req, res) => {
-  const { id } = req.params;
+  const cartItemId = req.params.id;
 
   const sql = 'DELETE FROM cartItems WHERE id=?;';
 
-  connection.query(sql, id, (err, results) => {
+  connection.query(sql, cartItemId, (err, results) => {
     if (err) {
       return res.status(StatusCodes.BAD_REQUEST).end();
     }
     return res.status(StatusCodes.OK).json(results);
   });
+};
+
+const ensureAuthorization = (req) => {
+  const receivedJwt = req.headers.authorization;
+  const decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+  return decodedJwt;
 };
 
 module.exports = {
