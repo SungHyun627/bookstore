de4c960d9d56b5793219814edef8e7bca984acbe
Sunghyun627 <kksscc4@gmail.com>
2024-01-22 13:25:28 +0900

    feat: 좋아요 유효성 검사 구현

--- middlewares/likemiddleware.js
+++ middlewares/likemiddleware.js
@@ -1,4 +1,4 @@
-const { validationResult, body } = require('express-validator');
+const { validationResult, params } = require('express-validator');
 const { StatusCodes } = require('http-status-codes');
 
 const validateEachValidation = (req, res, next) => {
@@ -10,22 +10,15 @@ const validateEachValidation = (req, res, next) => {
   return res.status(StatusCodes.BAD_REQUEST).json(err.array());
 };
 
-const validateBookId = body('book_id')
+const validateLikedBookId = params('id')
   .trim()
   .notEmpty()
   .withMessage('도서 아이디를 입력해주세요')
   .isInt()
   .withMessage('도사 아이디는 숫자여야 합니다.');
 
-const validateQuantity = body(`quantity`)
-  .trim()
-  .notEmpty()
-  .withMessage('구매 수량을 입력해주세요.')
-  .isInt()
-  .withMessage('구매 수량은 숫자여야 합니다.');
-
-const validateAddToCart = [validateBookId, validateQuantity, validateEachValidation];
+const validateLike = [validateLikedBookId, validateEachValidation];
 
 module.exports = {
-  validateAddToCart,
+  validateLike,
 };
--- routes/likes.js
+++ routes/likes.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const { addLike, removeLike } = require('../controllers/likeController');
+const { validateLike } = require('../middlewares/likemiddleware');
 
 router.use(express.json());
 
-router.route('/:id').post(addLike).delete(removeLike);
+router.route('/:id').post(validateLike, addLike).delete(validateLike, removeLike);
 
 module.exports = router;
